22ccbbea58ab8e58f7e5c830c49d533529544ac8
Priyanshu D Sharma <89120960+priyanshujiiii@users.noreply.github.com>
2022-08-28 15:42:17 +0530

    Create Lect_1.cpp

---
+++ Lect_1.cpp
@@ -0,0 +1,38 @@
+// Understanding Algorithms
+//>> Correctness
+//>> Efficiency
+//>>>> Asympotic complexity, O() notation
+//>> Modelling
+//>>>  Graphs, data structures , decomposing the problem
+//>> Techniques
+//>> Divide and conquer, greedy, dynamic programming
+
+// Expectations
+//>> Background in programming
+//>>>> launguage c++
+//>> Basic Data Structures
+//>>>> Arrays,lists
+
+// Topics to be covered
+//>> Asympotic complexity
+//>> Searching and sorting in arrays.
+//>>>> Binary search ,insertion sort, selection sort,merge sort, quick sort
+//>> Graphs and graph algorithms
+//>>>> Representation , reachibility , connectedness
+//>>>> Directed asyclic graphs
+//>>>> Shortest paths , spanning trees
+//>> Algortihms design techniques 
+//>>>> Divide and conquer , Greedy algorithms, Dynamic Programming
+//>> Data structures
+//>>>> Priority queues/heaps, Search trees, Union of disjoint sets(union-find)
+//>> Miscellaneous
+//>>>> Intracability,...
+
+//>> Section 1: Motivation, asymptotic complexity
+//>> Section 2: Searching and sorting
+//>> Section 3: Graphs and basic graph algortihms
+//>> Section 4: More Graph algorithms , disjoint set
+//>> Section 5: Divide and conquer, heaps
+//>> Section 6: Search trees , greedy algorithms
+//>> Section 7: Dynamic programming
+//>> Section 8: Miscallaneous topics.
